b58e0d0a72298f43fac07f941098e838a7849a31
lm-loris <58504598+lm-loris@users.noreply.github.com>
2023-09-19 14:50:06 +0200

    Changement de texte

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur le blind test</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
+        <p>
+          Il va falloir modifier le code pour faire un vrai blind test okay ?!
+        </p>
       </div>
       <div className="App-buttons"></div>
     </div>
